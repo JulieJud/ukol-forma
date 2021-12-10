34e82f208a0527586469f2201bdada95ca4c61ce
Julie Judachina <iuliia.saunova@email.cz>
2021-12-10 23:39:27 +0100

    add basic form

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import useSignUpForm from "./CustomHooks";
+
+const Signup = () => {
+  const { inputs, handleInputChange, handleSubmit } = useSignUpForm();
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Email Address</label>
+        <input
+          type="email"
+          name="email"
+          onChange={handleInputChange}
+          value={inputs.email}
+          required
+        />
+      </div>
+      <div>
+        <label>First Name</label>
+        <input
+          type="text"
+          name="firstName"
+          onChange={handleInputChange}
+          value={inputs.firstName}
+          required
+        />
+      </div>
+
+      <div>
+        <label>Password</label>
+        <input
+          type="password"
+          name="password1"
+          onChange={handleInputChange}
+          value={inputs.password1}
+        />
+      </div>
+      <div>
+        <label>Re-enter Password</label>
+        <input
+          type="password"
+          name="password2"
+          onChange={handleInputChange}
+          value={inputs.password2}
+        />
+      </div>
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
+export default Signup;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Signup from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Signup />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
